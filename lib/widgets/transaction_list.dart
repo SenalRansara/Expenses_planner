75a1f3be00d4cb0bc0a34fe2d23b32ac5dae111d
SenalRansara <senal.ransara38@gmail.com>
2022-05-25 16:45:56 +0530

    date picker added

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -22,12 +22,12 @@ class TransactionList extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Container(
-                    height: 200,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    )),
+                // Container(
+                //     height: 200,
+                //     child: Image.asset(
+                //       'assets/images/waiting.png',
+                //       fit: BoxFit.cover,
+                //     )),
               ],
             )
           : ListView.builder(
